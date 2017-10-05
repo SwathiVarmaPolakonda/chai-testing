cff9211c03c815a23067aa68ed1d969a55bbf2db
SwathiVarmaPolakonda <swathivarmaps@gmail.com>
2017-10-05 13:35:35 +0530

    changedthe message

--- inheritance/test/test.js
+++ inheritance/test/test.js
@@ -4,7 +4,7 @@ var expect = chai.expect;
 var { inheritance } = require("../inherit");
 var assert = require("assert");
 describe("Inheritance", function() {
-  it("Should calculate area of rectangle and square", function() {
+  it("Should return 12", function() {
     assert.equal(area(3, 4), 12);
   });
 });
